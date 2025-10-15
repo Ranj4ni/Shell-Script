a3218b66dbc4376ee2012a32a2e78b6d5ea23168
Ranj4ni <ranjani.r4j@gmail.com>
2025-10-15 15:04:41 +0530

    second commit

--- variable.sh
+++ variable.sh
@@ -2,3 +2,7 @@
 echo "file name is $0"
 echo "my name is $1"
 echo "my age is $2"
+echo "my city is $3"
+echo "total number of arguments is $#"
+echo "all arguments are $*"
+echo "all arguments are $@"
