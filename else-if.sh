7d3dda2f66e5f88616a7767f6270b6c3f789597d
Ranj4ni <ranjani.r4j@gmail.com>
2025-10-15 22:37:45 +0530

    sixth commit

---
+++ else-if.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+read -p "Enter the number: " number
+if [ $number -gt 10 ]
+then
+   echo "The number is greater than 10"
+elif [ $number -eq 10 ]
+then
+   echo "The number is equal to 10"
+else
+   echo "The number is less than 10"
+fi
