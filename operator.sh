1312d2d571b5c2fc3852903074cc76c298bedb16
Ranj4ni <ranjani.r4j@gmail.com>
2025-10-15 17:31:38 +0530

    third commit

---
+++ operator.sh
@@ -0,0 +1,10 @@
+a=20
+b=30
+echo "the value of a is $a and b is $b"
+echo "the sum is `expr $a + $b`"
+echo "the difference is `expr $b - $a`"
+echo "the product is `expr $a \* $b`"
+echo "the quotient is `expr $b / $a`"
+echo "the remainder is `expr $b % $a`"
+a=$b
+echo the value of a is $a and b is $b
